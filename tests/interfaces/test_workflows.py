ddc14cf8b87fa34082f7a1a459eb122d0c86904c
s1111303 <marta.pasquini@syngenta.com>
2022-11-08 11:00:10 +0100

    adjusting tests to use conftest fixtures

--- tests/interfaces/test_workflows.py
+++ tests/interfaces/test_workflows.py
@@ -7,18 +7,18 @@ import unittest.mock
 
 
 @unittest.mock.patch('linchemin.IO.io.write_json')
-def test_workflow_basic(mock_os):
-    path = "../test_file/az_retro_output_raw.json"
-    input_dict = {path: 'az'}
+def test_workflow_basic(mock_os, az_path, ibm2_path):
+    az_file = str(az_path)
+    ibm2_file = str(ibm2_path)
+    input_dict = {az_file: 'az'}
     out = process_routes(input_dict)
     assert out
 
     routes, meta = facade('translate', 'syngraph', out.routes_list, 'noc', out_data_model='bipartite')
     mock_os.assert_called_with(routes, "routes.json")
 
-    path2 = "../cgu/data/ibm_output2.json"
-    input_dict_multicasp = {path: 'az',
-                            path2: 'ibmrxn'}
+    input_dict_multicasp = {az_file: 'az',
+                            ibm2_file: 'ibmrxn'}
     out = process_routes(input_dict_multicasp)
     assert out
 
@@ -27,13 +27,13 @@ def test_workflow_basic(mock_os):
 
     # error raised for invalid casp
     with pytest.raises(KeyError) as ke:
-        input_dict2 = {path: 'wrong_casp'}
+        input_dict2 = {az_path: 'wrong_casp'}
         process_routes(input_dict2, output_format='json')
     assert "KeyError" in str(ke.type)
 
     # error raised for invalid output format
     with pytest.raises(KeyError) as ke:
-        input_dict = {path: 'az'}
+        input_dict = {az_file: 'az'}
         process_routes(input_dict, output_format='jpg')
     assert "KeyError" in str(ke.type)
 
@@ -45,8 +45,8 @@ def test_workflow_basic(mock_os):
 
 @unittest.mock.patch('linchemin.IO.io.dict_list_to_csv')
 @unittest.mock.patch('linchemin.IO.io.dataframe_to_csv')
-def test_workflow_metric(mock_dataframe, mock_csv):
-    path = "../test_file/ibm_output2.json"
+def test_workflow_metric(mock_dataframe, mock_csv, ibm2_path):
+    path = str(ibm2_path)
     input_dict = {path: 'ibmrxn'}
     out = process_routes(input_dict, output_format='csv', functionalities=['compute_descriptors'],
                          descriptors=['branching_factor', 'nr_steps'],
@@ -57,8 +57,8 @@ def test_workflow_metric(mock_dataframe, mock_csv):
 
 
 @unittest.mock.patch('linchemin.interfaces.workflows.write_syngraph')
-def test_merging(mock_writer1):
-    path = "../test_file/az_retro_output_raw.json"
+def test_merging(mock_writer1, az_path):
+    path = str(az_path)
     input_dict = {path: 'az'}
     out = process_routes(input_dict, output_format='png', functionalities=['merging'])
     mock_writer1.assert_called_with(out.routes_list, 'bipartite', 'png', 'routes')
@@ -66,8 +66,8 @@ def test_merging(mock_writer1):
 
 @unittest.mock.patch('linchemin.IO.io.dict_list_to_csv')
 @unittest.mock.patch('linchemin.IO.io.dataframe_to_csv')
-def test_workflow_cluster_dist_matrix(mock_dataframe, mock_csv):
-    path = "../test_file/askos_output.json"
+def test_workflow_cluster_dist_matrix(mock_dataframe, mock_csv, mit_path):
+    path = str(mit_path)
     input_dict = {path: 'askcos'}
     out = process_routes(input_dict, output_format='csv', functionalities=['clustering'])
     routes, meta = facade('translate', 'syngraph', out.routes_list, 'noc', out_data_model='bipartite')
@@ -76,8 +76,8 @@ def test_workflow_cluster_dist_matrix(mock_dataframe, mock_csv):
 
 
 @unittest.mock.patch('linchemin.IO.io.write_nx_to_graphml')
-def test_workflow_graphml(mock_graphml):
-    path = "../test_file/askos_output.json"
+def test_workflow_graphml(mock_graphml, mit_path):
+    path = str(mit_path)
     input_dict = {path: 'askcos'}
     out = process_routes(input_dict, output_format='graphml')
     routes, meta = facade('translate', 'syngraph', out.routes_list, 'noc', out_data_model='bipartite')
@@ -94,8 +94,8 @@ def test_helper(capfd):
 
 @unittest.mock.patch('linchemin.IO.io.dict_list_to_csv')
 @unittest.mock.patch('linchemin.IO.io.write_json')
-def test_reaction_strings_extraction(mock_json, mock_csv):
-    path = "../test_file/az_retro_output_raw.json"
+def test_reaction_strings_extraction(mock_json, mock_csv, az_path):
+    path = str(az_path)
     input_dict = {path: 'az'}
     out = process_routes(input_dict, output_format='csv', functionalities=['extracting_reactions'])
     routes, meta = facade('translate', 'syngraph', out.routes_list, 'noc', out_data_model='bipartite')
@@ -105,8 +105,8 @@ def test_reaction_strings_extraction(mock_json, mock_csv):
 
 @unittest.mock.patch('linchemin.IO.io.dict_list_to_csv')
 @unittest.mock.patch('linchemin.IO.io.write_json')
-def test_graphml(mock_json, mock_csv):
-    path = "../test_file/az_retro_output_raw.json"
+def test_graphml(mock_json, mock_csv, az_path):
+    path = str(az_path)
     input_dict = {path: 'az'}
     out = process_routes(input_dict, output_format='csv', functionalities=['extracting_reactions'])
     routes, meta = facade('translate', 'syngraph', out.routes_list, 'noc', out_data_model='bipartite')
@@ -114,8 +114,8 @@ def test_graphml(mock_json, mock_csv):
     mock_json.assert_called_with(out.reaction_strings, "reaction_strings.json")
 
 
-def test_full_workflow():
-    path = "../test_file/az_retro_output_raw.json"
+def test_full_workflow(az_path):
+    path = str(az_path)
     input_dict = {path: 'az'}
     process_routes(input_dict, out_data_model='monopartite_reactions',
                    functionalities=['compute_descriptors', 'clustering_and_d_matrix', 'merging'])
